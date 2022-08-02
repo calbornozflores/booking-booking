069ac297e1fb29324caa2d3f8331bf6106c3b39d
Claudio Albornoz <c.albornoz.flores@gmail.com>
2022-08-01 22:23:09 -0400

    init formatting

---
+++ tools/format.py
@@ -0,0 +1,68 @@
+extra_info_substr = [
+    "Breakfast included",
+    "Travel Sustainable property",
+    "Location",
+    "Getaway Deal",
+    "Duplex Apartment",
+    "Free cancellation",
+    "FREE cancellation",
+    "left at this price on our site",
+]
+
+rank_description = [
+    "New to Booking.com",
+    "Exceptional",
+    "Wonderful",
+    "Excellent",
+    "Very Good",
+    "Good",
+]
+
+matchStrField = {
+    "center": "relative_location",
+    "nights": "nights",
+    "review": "reviews",
+    "taxes": "taxes",
+}
+
+
+def formatting():
+    file = open('output/research.csv', 'r', newline ='')
+    lines = file.readlines()
+    df_list = []
+    for line_list in lines:
+        hotelInfo = {}
+        line_list = line_list.split(";")[:-1]
+        line_list = [ele for ele in line_list if ele != ""]
+        line_list = [ele for ele in line_list if not any(ext in ele for ext in extra_info_substr)]
+        if len(line_list) > 0:
+            hotelInfo["hotel_name"] = line_list[0]
+            try:
+                rankDescriptionField = [x for x in line_list if any(rd in x for rd in rank_description)][0]
+                if any(c.isdigit() for c in rankDescriptionField):
+                    rankDesciption, rankNumber = rankDescriptionField.split(" ")
+                    hotelInfo["rank_description"] = rankDesciption
+                    try:
+                        hotelInfo["rank_value"] = int(rankNumber)
+                    except:
+                        pass
+                else:
+                    try:
+                        hotelInfo["rank_value"] = int(line_list[ line_list.index(rankDescriptionField) - 1 ])
+                    except:
+                        pass
+            except:
+                pass
+            for k in matchStrField.keys():
+                try:
+                    xField = [x for i, x in enumerate(line_list) if k in x][0]
+                    hotelInfo[matchStrField[k]] = xField
+                    if k == "taxes":
+                        hotelInfo["price"] = line_list[ line_list.index(xField) - 1 ]
+                except:
+                    pass
+            df_list.append(hotelInfo)
+        print(hotelInfo)
+    file.close()
+
+formatting()
